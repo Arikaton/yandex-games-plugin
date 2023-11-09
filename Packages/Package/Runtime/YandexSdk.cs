b14fd3b943f9738a1e7d3f25feaa39178ead6aac
Stanislav Ryzhov <stanislav.ryzhov@tripledotstudios.com>
2023-11-09 19:37:14 +0300

    Added few files to package

---
+++ Packages/Package/Runtime/YandexSdk.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Arikaton.YandexGames
+{
+    public class YandexSdk
+    {
+        public static void Init()
+        {
+            Debug.Log("Yandex sdk initialized");
+        }
+    }
+}
